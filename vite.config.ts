4d2befc71e2388fccddf4c3fb1a74c36ea9e09d5
HarryOnBreadboard <a.leroux330@laposte.net>
2025-09-12 21:48:00 +0200

    Revert to blank

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,9 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 const root = resolve(__dirname, "src");
-const outDir = resolve(__dirname, "dist");
 
 // https://vite.dev/config/
 export default defineConfig({
   root,
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir,
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(root, "index.html"),
-        about: resolve(root, "about", "index.html"),
-      },
-    },
-  },
 });
